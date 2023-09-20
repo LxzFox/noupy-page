8e80b9f451e1f1ff484f89eca30a1fd182fe4254
USanchez <ulissesaxel.sc@gmail.com>
2023-09-19 19:12:28 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var check = document.querySelector(".check");
+
+// Cuando se carga la página, verifica si hay un valor en localStorage para el estado del checkbox
+window.addEventListener('load', function () {
+    var isChecked = localStorage.getItem("checkbox_state");
+    if (isChecked === "true") {
+        check.checked = true;
+    }
+});
+
+// Cuando se hace clic en el checkbox, guarda su estado en localStorage
+check.addEventListener('click', function () {
+    localStorage.setItem("checkbox_state", check.checked);
+    idioma();
+});
+
+function idioma() {
+    var id = check.checked;
+    if (id == true) {
+        location.href = "en/index.html";
+    } else {
+        location.href = "/index.html";
+    }
+}
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
